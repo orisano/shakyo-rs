4e839bdb70886b57d53866129b47698b2dfc820e
Nao Yonashiro <owan.orisano@gmail.com>
2017-04-26 22:44:58 +0900

    ベクター,要素へのアクセス

---
+++ 1.6/4.16/vector.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+    println!("The third element of v is {}", v[2]);
+
+    let v = vec![0; 10];
+    println!("The third element of v is {}", v[2]);
+}
