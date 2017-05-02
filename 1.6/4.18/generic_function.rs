85c24d982f5fda716be74a5de25b69a1554df837
Nao Yonashiro <owan.orisano@gmail.com>
2017-05-02 23:02:22 +0900

    ジェネリック関数

---
+++ 1.6/4.18/generic_function.rs
@@ -0,0 +1,11 @@
+fn main() {
+    fn takes_anything<T>(x: T) {
+        // ...
+    }
+    fn takes_two_of_the_same_things<T>(x: T, y: T) {
+        // ...
+    }
+    fn takew_two_thing<T, U>(x: T, y: U) {
+        // ...
+    }
+}
