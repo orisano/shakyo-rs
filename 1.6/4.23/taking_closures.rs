2d5fe691ee8c068100f7c4267bf0c6a180e9f18b
Nao Yonashiro <owan.orisano@gmail.com>
2017-05-07 23:23:59 +0900

    クロージャを引数に取る

---
+++ 1.6/4.23/taking_closures.rs
@@ -0,0 +1,21 @@
+fn main() {
+    {
+        fn call_with_one<F>(some_closure: F) -> i32
+            where F : Fn(i32) -> i32 {
+            some_closure(1)
+        }
+
+        let answer = call_with_one(|x| x + 2);
+
+        assert_eq!(3, answer);
+    }
+    {
+        fn call_with_one(some_closure: &Fn(i32) -> i32) -> i32 {
+            some_closure(1)
+        }
+
+        let answer = call_with_one(&|x| x + 2);
+
+        assert_eq!(3, answer);
+    }
+}
